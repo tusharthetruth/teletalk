5bdabd0037e129aa67656b5e60c2e1273146b897
tusharnigam88@gmail.com <tusharnigam88@gmail.com>
2020-07-10 21:49:58 +0530

    fixed issue

--- vector/src/chatapp/java/com/chatapp/ProfileSetActivity.java
+++ vector/src/chatapp/java/com/chatapp/ProfileSetActivity.java
@@ -2,12 +2,15 @@ package com.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceManager;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import im.vector.R;
+import im.vector.util.PreferencesManager;
 
-public class ProfileSetActivity extends AppCompatActivity {
+public class ProfileSetActivity extends AppCompatActivity implements ISyncListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,30 @@ public class ProfileSetActivity extends AppCompatActivity {
         toolbar.setTitle("Profile");
         getSupportActionBar().setTitle("Profile");
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new ProfileUpdateFragment()).commit();
+        if (!PreferenceManager.getDefaultSharedPreferences(this).getBoolean(PreferencesManager.IS_SYNC_DIALOG_SHOWN, false))
+            showSyncDialog();
     }
 
     @Override
     public void onBackPressed() {
         setResult(RESULT_OK);
         super.onBackPressed();
+    }
+    private void showSyncDialog() {
+        ISyncListener listener = this;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SyncFragment f = new SyncFragment();
+                f.isyncListener = listener;
+                f.show(getSupportFragmentManager());
+            }
+        }, 200);
+
+    }
+
+    @Override
+    public void onSyncEnableClick() {
+
     }
 }
