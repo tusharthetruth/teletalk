79ea8aa7420c6691bd3dcc2753f5a7b348034224
tusharnigam88@gmail.com <tusharnigam88@gmail.com>
2020-09-14 23:41:09 +0530

    updated for overdraw permission

--- vector/src/chatapp/java/com/chatapp/ChatMainActivity.java
+++ vector/src/chatapp/java/com/chatapp/ChatMainActivity.java
@@ -189,7 +189,28 @@ public class ChatMainActivity extends VectorAppCompatActivity implements View.On
         if (CommonActivityUtils.isGoingToSplash(this)) {
             return;
         }
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!android.provider.Settings.canDrawOverlays(this)) {
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                                startActivityForResult(intent, 0);
+                                break;
 
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                //No button clicked
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("App requires \"Draw over other apps\" permission to display incoming call. So could you please provide the permission?").setPositiveButton("Yes", dialogClickListener)
+                        .setNegativeButton("No", dialogClickListener).setCancelable(false).show();
+            }
+        }
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         sharedInstance = this;
